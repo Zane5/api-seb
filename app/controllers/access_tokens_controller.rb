42c03e554cfc9dc3e8061527a8c35e8bcd4fcc37
vagrant <vagrant@x.dev>
2020-05-28 12:19:41 +0000

    login user endpoint

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,11 +1,7 @@
 class AccessTokensController < ApplicationController
   def create
-    error = {
-      "status" => "401",
-      "source" => { "pointer" => "/code" },
-      "title" => "Authentication code is invalid",
-      "detail" => "You must provide valid code in order to exchange it for token.",
-    }
-    render json: { "errors": [error] }, status: 401
+    authenticator = UserAuthenticator.new(params[:code])
+    authenticator.perform
+    authentication_error
   end
 end
