15db07b410c56f89849b3b9f7acf698d8b32e69e
vagrant <vagrant@x.dev>
2020-05-28 12:02:45 +0000

    accesstokenController return 401

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -1,2 +1,11 @@
 class AccessTokensController < ApplicationController
+  def create
+    error = {
+      "status" => "401",
+      "source" => { "pointer" => "/code" },
+      "title" => "Authentication code is invalid",
+      "detail" => "You must provide valid code in order to exchange it for token.",
+    }
+    render json: { "errors": [error] }, status: 401
+  end
 end
