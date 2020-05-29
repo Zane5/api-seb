28fa74b6c2c5d2f617aa5f98994ca40165bf9a14
vagrant <vagrant@x.dev>
2020-05-29 04:26:06 +0000

    shared examples

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe AccessTokensController, type: :controller do
   describe "#create" do
-    context "when invalid request" do
+    shared_examples_for "unauthorized_requests" do
       let(:error) do
         {
           "status" => "401",
@@ -21,6 +21,16 @@ RSpec.describe AccessTokensController, type: :controller do
         post :create
         expect(json["errors"]).to include(error)
       end
+    end
+
+    context "when no code provided" do
+      subject { post :create }
+      it_behaves_like "unauthorized_requests"
+    end
+
+    context "when invalid code provided" do
+      subject { post :create, params: { code: "invalid_code" } }
+      it_behaves_like "unauthorized_requests"
     end
 
     context "when success request" do
