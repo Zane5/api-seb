6321c3ea6568773b06052882b0ee402a20138255
vagrant <vagrant@x.dev>
2020-05-29 05:12:01 +0000

    create sessiion

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,6 +2,8 @@ class AccessTokensController < ApplicationController
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
-    authentication_error
+    # authentication_error
+
+    render json: authenticator.access_token, status: :created
   end
 end
